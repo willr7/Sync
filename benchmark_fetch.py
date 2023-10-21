bc17741509ea48bc4c4bf5ff3fff5e529b7130b6
ShonellR <shonrow04@gmail.com>
2023-10-21 17:51:01 -0400

    Adding things
    CPU, GPU benchmarks, Game id and spec fetching

---
+++ benchmark_fetch.py
@@ -0,0 +1,124 @@
+import json
+import urllib3
+from html.parser import HTMLParser
+
+class BenchmarkParser(HTMLParser):
+    #Specifically designed for the common benchmarks site
+    
+    def __init__(self, *, convert_charrefs: bool = True) -> None:
+        self.scores = [] #dictionary of parts to scores
+        self.in_visible = False
+        self.in_entry = False
+        self.in_table = False
+        self.done = False
+        self.current_entry = {}
+        self.current_attr = ""
+        self.item_type = {"prdname", "count", "price-neww"}
+        super().__init__(convert_charrefs=convert_charrefs)
+        
+    def handle_starttag(self, tag, attrs):
+        if tag == 'div' and attrs == [('id', 'mark')]:
+            self.in_visible = True
+        elif tag == 'div' and attrs == [('id', 'value')]:
+            self.in_visible = False
+        elif tag == 'ul' and attrs == [('class', 'chartlist')]: #[tag, attrs] == ['ul', ('class', 'chartlist')]:
+            self.in_table = True
+        elif self.in_table and tag == 'a':
+            self.in_entry = True
+        elif self.in_entry and tag == 'span':
+            self.current_attr = attrs[0][1] # second element of the first tuple
+            
+    
+    def handle_endtag(self, tag):
+        if tag == 'ul' and self.in_table:
+            self.done = True
+            self.in_table = False
+        elif tag == 'a' and self.in_table and self.in_entry and not self.done:
+            self.in_entry = False
+            # print(self.current_entry)
+            self.scores.append(dict(self.current_entry))
+
+    def handle_data(self, data):
+        if self.in_table and self.in_entry and self.current_attr in self.item_type:
+            self.current_entry[self.current_attr] = data
+            
+    def clear_scores(self):
+        self.scores = []
+        
+class GPUParser(HTMLParser):
+    def __init__(self, *, convert_charrefs: bool = True) -> None:
+        self.scores = [] #dictionary of parts to scores
+        self.done = False
+        self.in_table = False
+        self.in_entry = False
+        self.current_key = ""
+        self.item_type = {"prdname", "count", "price-neww"}
+        self.current_entry = []
+        super().__init__(convert_charrefs=convert_charrefs)
+        
+    def handle_starttag(self, tag, attrs):   
+        if tag == 'ul' and attrs == [('class', 'chartlist')]:
+            self.in_table = True
+        elif tag == 'li' and self.in_table:
+            self.in_entry = True
+        elif tag == 'span' and self.in_table and self.in_entry:
+            self.current_key = attrs[0][1]
+    
+    def handle_endtag(self, tag):
+        if tag == 'ul' and self.in_table:
+            self.in_table = False
+            self.done = True
+        elif tag == 'li' and self.in_entry and self.current_entry:
+            self.in_entry = False
+            self.scores.append(self.current_entry)
+            self.current_entry = []
+        
+    
+    def handle_data(self, data):
+        if self.current_key in self.item_type and not self.done:
+            self.current_entry.append(data)
+            self.current_key = ""
+        
+
+
+if __name__ == "__main__":
+    cpu_benchmark_url = "https://www.cpubenchmark.net/desktop.html"
+    gpu_benchmark_url = "https://www.videocardbenchmark.net/common_gpus.html"
+    
+    cpu_resp = urllib3.request("GET", cpu_benchmark_url)
+    gpu_resp = urllib3.request("GET", gpu_benchmark_url)
+    
+    if cpu_resp.status != 200 or gpu_resp.status != 200:
+        print("Error Getting Benchmark Url results")
+        exit()
+        
+    parser = BenchmarkParser()
+    parser.feed(str(cpu_resp.data))
+    
+    cpu_dict = {}
+    for elem in parser.scores:
+        name, score, price = elem['prdname'], elem['count'], elem['price-neww']
+        name = name[:name.find('@')].strip() if name.find('@') != -1 else name.strip()
+        score = int(score.replace(',', ''))
+        price = int(price.strip("$").strip("*").replace(',', '').replace('.', '')) if price != 'NA' else 'NA'
+        cpu_dict[name] = (score, price)
+       
+    parser.clear_scores() 
+    
+    parser = GPUParser()
+    parser.feed(str(gpu_resp.data))
+    
+    gpu_dict = {}
+    for elem in parser.scores:
+        name, score, price = elem
+        name = name[:name.find('@')].strip() if name.find('@') != -1 else name.strip()
+        score = int(score.replace(',', ''))
+        price = int(price.strip("$").strip("*").replace(',', '').replace('.', '')) if price != 'NA' else 'NA'
+        gpu_dict[name] = (score, price)
+    
+    f = open("cpu_benchmarks.json", "w")
+    json.dump(cpu_dict, f, indent=6)
+    f = open("gpu_benchmarks.json", "w")
+    json.dump(gpu_dict, f, indent=6)
+
+    
---
+++ steam_game_id.py
@@ -0,0 +1,75 @@
+import json
+import urllib3
+from html.parser import HTMLParser
+
+
+class SpecParser(HTMLParser):
+    #state machine to get the specs from HTML
+    def __init__(self, *, convert_charrefs: bool = True) -> None:
+        self.in_spec = False #whether or not the next data option should be added to the dictionary
+        self.in_items = False
+        self.current_key = ""
+        self.spec_dict = {}
+        super().__init__(convert_charrefs=convert_charrefs)
+        
+    def handle_starttag(self, tag, attrs):
+        if tag == "strong":
+            self.in_spec = True
+
+    def handle_endtag(self, tag):
+        if tag == "strong":
+            self.in_spec = False
+            self.in_items = True
+        elif self.in_items:
+            self.in_items = False
+
+    def handle_data(self, data):
+        if self.in_spec:
+            self.current_key = data.strip(':')
+        elif self.in_items and self.current_key != "Minimum" and self.current_key != "Recommended":
+            if self.current_key == "Processor" or self.current_key == "Graphics":
+                text = data.strip()
+                self.spec_dict[self.current_key] = text.split(" or ")
+            else:
+                self.spec_dict[self.current_key] = data.strip()
+
+if __name__ == "__main__":
+    
+    game_dict = {}
+    source_filename = "ids.json"
+    dest_filename = "game_ids.json"
+    
+    try:    
+        f = open(dest_filename, "x", encoding='utf-8')
+        json_dict = json.load(open(source_filename, encoding='utf-8'))
+        #I want a set of all names, dictionary name : id
+        for pair in json_dict['applist']['apps']:
+            game_dict[pair['name']] = pair['appid']
+        json.dump(game_dict, f, indent=6)
+    except FileExistsError:
+        game_dict = json.load(open(dest_filename))
+    
+    game_name = "Rocket League" #will be gotten from user
+    game_id = game_dict[game_name]
+    
+    # App details API: 
+    app_url = "http://store.steampowered.com/api/appdetails?appids="
+    resp = urllib3.request("GET", app_url + str(game_id) + "&l=english")
+    OK = 200 #error code
+    if resp.status == OK:
+        print("request: success")
+    
+    game_data_json = resp.data
+    game_data = json.loads(game_data_json)
+    game_specs_dict = game_data[str(game_id)]['data']['pc_requirements']
+    
+    parser = SpecParser()
+    parser.feed(game_specs_dict['minimum'])
+    minimum_specs = dict(parser.spec_dict)
+    parser.feed(game_specs_dict['recommended'])
+    recommened_specs = parser.spec_dict
+    
+    Specifications = {"Minimum": minimum_specs,
+                    "Recommended": recommened_specs}
+    
+    print(Specifications) #this info can be returned.
